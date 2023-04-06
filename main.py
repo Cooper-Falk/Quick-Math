5e65ba03fe8597bb6269b40c63484ff145896f7d
Cooper-Falk <57161597+Cooper-Falk@users.noreply.github.com>
2023-04-06 16:31:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,20 @@ mnist = tf.keras.datasets.mnist
 (xtrain, ytrain), (xtest, ytest) = mnist.load_data()
 xtrain = tf.keras.utils.normalize(xtrain, axis=1)
 xtest = tf.keras.utils.normalize(xtest, axis=1)
+# xtrain = xtrain.reshape(60000, 784).astype("float32") / 255
+# xtest = xtest.reshape(10000, 784).astype("float32") / 255
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-model.add(tf.keras.layers.Dense(128, activation="relu"))
-model.add(tf.keras.layers.Dense(128, activation="relu"))
-model.add(tf.keras.layers.Dense(10, activation="softmax"))
-model.compile(optimizer="Adamax", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-model.fit(xtrain, ytrain, epochs=3)
-model.save("first.model")
+# model = tf.keras.models.Sequential()
+# model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+# model.add(tf.keras.layers.Dense(128, activation="relu"))
+# model.add(tf.keras.layers.Dense(128, activation="relu"))
+# model.add(tf.keras.layers.Dense(10, activation="softmax"))
+# model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+# model.fit(xtrain, ytrain, epochs=30)
+# model.save("first.model")
+#
+# results = model.evaluate(xtest, ytest, batch_size=128)
+
 currentmodel = tf.keras.models.load_model("first.model")
 
 current = 1
@@ -27,12 +32,12 @@ while os.path.isfile(f"numberpictures/number{current}.png"):
     img_array = np.invert(np.array(img))
     img_array = img_array.reshape(1, 28, 28)
     img_array = img_array / 255.0
-    # for x in range(28):
-    #     for y in range(28):
-    #         if img_array[0, x, y] > 0:
-    #             img_array[0, x, y] = 1
-    #         else:
-    #             img_array[0, x, y] = 0
+    for x in range(28):
+        for y in range(28):
+            if img_array[0, x, y] > 0:
+                img_array[0, x, y] = 1
+            else:
+                img_array[0, x, y] = 0
     # img.show()
     prediction = currentmodel.predict(img_array)
     number = np.argmax(prediction)
